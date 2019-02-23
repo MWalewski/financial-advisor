b04838d4dc0edd6830f51344786ff8d843c3d0d7
Maciej <mjwalewski1@gmail.com>
2019-02-23 09:27:26 +0100

    Product section completed

--- scripts/temp/index.js
+++ scripts/temp/index.js
@@ -5,3 +5,48 @@ menuToggler.addEventListener("click", () => {
   navList.classList.toggle("navbar__nav--show");
   menuToggler.classList.toggle("navbar__menu-toggler--close");
 });
+
+const box1 = document.querySelector(".product__icon-box-1");
+const box2 = document.querySelector(".product__icon-box-2");
+const box3 = document.querySelector(".product__icon-box-3");
+
+const textBox1 = document.querySelector(".product__text-box-1");
+const textBox2 = document.querySelector(".product__text-box-2");
+const textBox3 = document.querySelector(".product__text-box-3");
+
+const title1 = document.querySelector(".product__icon-title-1");
+const title2 = document.querySelector(".product__icon-title-2");
+const title3 = document.querySelector(".product__icon-title-3");
+
+box1.addEventListener("click", () => {
+  textBox2.classList.remove("product__text-box--active");
+  textBox3.classList.remove("product__text-box--active");
+  setTimeout(() => {
+    textBox1.classList.add("product__text-box--active");
+  }, 500);
+
+  title2.classList.remove("product__icon-title--active");
+  title3.classList.remove("product__icon-title--active");
+  title1.classList.add("product__icon-title--active");
+});
+box2.addEventListener("click", () => {
+  textBox1.classList.remove("product__text-box--active");
+  textBox3.classList.remove("product__text-box--active");
+  setTimeout(() => {
+    textBox2.classList.add("product__text-box--active");
+  }, 500);
+  title1.classList.remove("product__icon-title--active");
+  title3.classList.remove("product__icon-title--active");
+  title2.classList.add("product__icon-title--active");
+});
+box3.addEventListener("click", () => {
+  textBox1.classList.remove("product__text-box--active");
+  textBox2.classList.remove("product__text-box--active");
+  setTimeout(() => {
+    textBox3.classList.add("product__text-box--active");
+  }, 500);
+
+  title1.classList.remove("product__icon-title--active");
+  title2.classList.remove("product__icon-title--active");
+  title3.classList.add("product__icon-title--active");
+});
