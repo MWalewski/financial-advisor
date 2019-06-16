47958f83a36a9d25bff61b3fa59260e87e2665cd
Maciej <mjwalewski1@gmail.com>
2019-06-16 21:02:36 +0200

    Smooth scroll for navbar and buttons

--- scripts/temp/index.js
+++ scripts/temp/index.js
@@ -2,8 +2,13 @@ const menuToggler = document.querySelector(".navbar__menu-toggler");
 const navList = document.querySelector(".navbar__nav");
 
 menuToggler.addEventListener("click", () => {
-  navList.classList.toggle("navbar__nav--show");
-  menuToggler.classList.toggle("navbar__menu-toggler--close");
+  navList.classList.value.includes("navbar__nav--show")
+    ? navList.classList.remove("navbar__nav--show")
+    : navList.classList.add("navbar__nav--show");
+
+  menuToggler.classList.value.includes("navbar__menu-toggler--close")
+    ? menuToggler.classList.remove("navbar__menu-toggler--close")
+    : menuToggler.classList.add("navbar__menu-toggler--close");
 });
 
 // PRODUCT SECTION
@@ -40,4 +45,49 @@ function removeIconActive() {
   [].forEach.call(icons, el => {
     el.classList.remove("product__icon-title--active");
   });
+}
+
+//SMOOTH SCROLL
+
+const navLinks = document.querySelectorAll("a.link");
+const findOutMoreBtn = document.querySelector(".btn-primary");
+
+[].forEach.call(navLinks, el => {
+  el.addEventListener("click", navbarClick);
+});
+
+findOutMoreBtn.addEventListener("click", navbarClick);
+
+function navbarClick() {
+  smoothScroll(event);
+  //close menu
+  navList.classList.remove("navbar__nav--show");
+  menuToggler.classList.remove("navbar__menu-toggler--close");
+}
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetName = event.currentTarget.getAttribute("href");
+  const targetPosition = document.querySelector(targetName).offsetTop - 50;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1500;
+  let startTime = null;
+
+  window.requestAnimationFrame(animation);
+
+  function animation(currentTime) {
+    if (!startTime) startTime = currentTime;
+    const progres = currentTime - startTime;
+
+    window.scrollTo(0, ease(progres, startPosition, distance, duration));
+    if (progres < duration) window.requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t * t * t + b;
+    t -= 2;
+    return (-c / 2) * (t * t * t * t - 2) + b;
+  }
 }
